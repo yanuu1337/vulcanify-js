31c2d499b98144b5c1cf3fa44d778a565103b08e
yanuu1337 <yanuu.why@gmail.com>
2021-09-26 22:28:06 +0200

    chore: add vulcan_api_error msg

--- src/errors/Errors.js
+++ src/errors/Errors.js
@@ -2,6 +2,7 @@ const { register } = require("./VulcanifyError");
 const ErrorMessages = {
   FIREBASE_API_ERROR: (received) =>
     `Unexpected response status code from the Firebase API: ${received}`,
+  VULCAN_API_ERROR: (received) => `Unexpected response from the Vulcan API: ${received}`,
   INVALID_CONTENT_TYPE: (received, required) =>
     `Received an unexpected Content-Type header in the response: ${received} (expected ${required})`,
     INVALID_TOKEN_CODE: `Specify a valid and correct token code!`,
